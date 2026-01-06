39559f22c741a977b49310cb46f81a73fbc3ec05
stephenmutt8-cmd <stephenmutt8@gmail.com>
2026-01-06 19:42:48 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,10 @@
+// @dart=3.0
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const LumeApp());
-}
-
-class LumeApp extends StatelessWidget {
-  const LumeApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Lume Empire',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Lume Empire'),
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Icon(Icons.account_balance, size: 100, color: Colors.blue),
-            const SizedBox(height: 20),
-            const Text(
-              'Welcome to the Lume Empire',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 10),
-            Text('Created by: Stephen M. Makanga', 
-                 style: TextStyle(fontSize: 18, color: Colors.grey[700])),
-          ],
-        ),
-      ),
-    );
-  }
+  runApp(const MaterialApp(
+    home: Scaffold(
+      body: Center(child: Text('Lume Empire is Live')),
+    ),
+  ));
 }
